b12b80318b1abb2cc4082ac865e25620922cac7e
acc668 <116805246+acc668@users.noreply.github.com>
2024-06-17 21:33:12 -0700

    Add files via upload
    Exercise 2

--- calculator.js
+++ calculator.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>
 {
     res.send('Hello World');
+});
+
+app.get('/bmicalculator', (req, res) =>
+{
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) =>
+{
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+
+    const bmi = weight / (height * height);
+
+    res.send(`Your BMI is ${bmi.toFixed(2)}`);
 });
 
 app.listen(3000, () =>
